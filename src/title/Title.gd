5e7bf2fd69bd788099b4dc4b5aa2f747d2391d70
Michael Springer <mike@sprngr.com>
2019-10-06 14:18:46 -0400

    Core flow

--- src/title/Title.gd
+++ src/title/Title.gd
@@ -2,9 +2,8 @@ extends Control
 
 export (PackedScene) var next_scene
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	GameState.reset()
 	
 func _input(event):
 	if event.is_action_pressed("ui_accept"):
